7ff399090bd55992ee063f0b0ff8badf37891c2f
addonion <addonion@gmail.com>
2020-07-09 16:05:05 +0500

    fix

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    pathPrefix: '/blog/article/',
+    pathPrefix: '/',
 
     siteName: 'Carrot quest',
     titleTemplate: "%s",
